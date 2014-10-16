43f469b35cbb0ec5db5fde5721216a093209190d
fabianofnl <fabianofnl@f3d11776-44a7-e924-277a-5d69014b13de>
2014-10-16 01:35:24 +0000

    Implementado recurso de upload file, ainda não está 100%.

--- trunk/provasonline/src/main/java/br/com/sgpo/model/ProvasModel.java
+++ trunk/provasonline/src/main/java/br/com/sgpo/model/ProvasModel.java
@@ -3,6 +3,7 @@ package br.com.sgpo.model;
 import java.sql.SQLException;
 import java.util.List;
 
+import br.com.sgpo.dto.ApostilaDTO;
 import br.com.sgpo.dto.ProvaDTO;
 import br.com.sgpo.dto.QuestaoDTO;
 import br.com.sgpo.dto.TemaDTO;
@@ -34,5 +35,14 @@ public interface ProvasModel {
 			throws ClassNotFoundException, SQLException;
 
 	public void remover(ProvaDTO provaDTO) throws ClassNotFoundException,
+			SQLException;
+
+	public List<ApostilaDTO> listarApostilas(Integer offSet,
+			Integer recordPerPage) throws ClassNotFoundException, SQLException;
+
+	public List<ProvaDTO> listarProvasPorApostilas(Integer apostilaId)
+			throws ClassNotFoundException, SQLException;
+
+	public Integer getTotalRegistrosApostilas() throws ClassNotFoundException,
 			SQLException;
 }
