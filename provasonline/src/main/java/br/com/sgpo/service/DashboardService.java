e030fd0e25b4b3785bf38b40588958d753e5597c
fabianofnl <fabianofnl@f3d11776-44a7-e924-277a-5d69014b13de>
2014-10-28 00:05:56 +0000

    Implementando dashboard do colaborador, ainda não funcional

---
+++ provasonline/src/main/java/br/com/sgpo/service/DashboardService.java
@@ -0,0 +1,17 @@
+package br.com.sgpo.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.sgpo.dto.AgendaDTO;
+import br.com.sgpo.dto.ApostilaDTO;
+
+public interface DashboardService {
+
+	public List<AgendaDTO> listarAgendas(Integer matricula)
+			throws ClassNotFoundException, SQLException;
+
+	public List<ApostilaDTO> listarApostilas(Integer matricula)
+			throws ClassNotFoundException, SQLException;
+
+}
--- provasonline/src/main/webapp/secure/apostilasVincular.jsp
+++ provasonline/src/main/webapp/secure/apostilasVincular.jsp
@@ -1,5 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@taglib prefix="paginator" uri="/WEB-INF/tlds/paginator"%>
+<%@ taglib prefix="paginator" uri="/WEB-INF/tlds/paginator"%>
 <!DOCTYPE html>
 <html>
 <head>
