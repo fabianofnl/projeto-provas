d8e60bef7fd5c7839719b0514db912525fa45b88
fabianofnl <fabianofnl@f3d11776-44a7-e924-277a-5d69014b13de>
2015-01-29 01:39:25 +0000

    Adicionado funcionalidade de download de arquivos. Adicionado funcionalidade de direcionamento para página de prova (foi necessário utilizar SessionScope no ManagedBean do dashboard do colaborador. Feito demais ajustes

---
+++ trunk/provasonline/src/main/java/br/com/sgpo/controller/DashboardColManagedBean.java
@@ -0,0 +1,230 @@
+package br.com.sgpo.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.ChartSeries;
+
+import br.com.sgpo.dto.AgendaDTO;
+import br.com.sgpo.dto.ApostilaDTO;
+import br.com.sgpo.dto.FuncionarioDTO;
+import br.com.sgpo.dto.ProvaDTO;
+import br.com.sgpo.dto.ProvaRealizadaDTO;
+import br.com.sgpo.dto.QuestaoDTO;
+import br.com.sgpo.service.DashboardService;
+import br.com.sgpo.service.DashboardServiceImpl;
+
+/**
+ * @author Roseli
+ * 
+ */
+@ManagedBean
+@SessionScoped
+public class DashboardColManagedBean implements Serializable {
+
+	private static final long serialVersionUID = -8521165965503236100L;
+
+	private static final Logger LOG = Logger
+			.getLogger(DashboardColManagedBean.class);
+
+	private List<AgendaDTO> listaAgendas = new ArrayList<AgendaDTO>();
+	private List<QuestaoDTO> listaQuestoes = new ArrayList<QuestaoDTO>();
+	private ApostilaDTO apostilaSelecionada = new ApostilaDTO();
+	private ProvaDTO provaSelecionada = new ProvaDTO();
+	private Boolean barChartFlag = false;
+
+	private CartesianChartModel categoryModel;
+
+	@PostConstruct
+	public void carregarNotas() {
+
+		try {
+			DashboardService dashboardService = new DashboardServiceImpl();
+
+			FacesContext context = FacesContext.getCurrentInstance();
+			HttpSession session = (HttpSession) context.getExternalContext()
+					.getSession(true);
+			FuncionarioDTO funcionario = (FuncionarioDTO) session
+					.getAttribute("funcionario");
+
+			List<ProvaRealizadaDTO> listaProvasRealizadas = dashboardService
+					.listarProvasRealizadasPorMatricula(funcionario
+							.getMatricula());
+
+			LOG.info(String.format("Quantidade de provas realizadas [%d]",
+					listaProvasRealizadas.size()));
+
+			categoryModel = new CartesianChartModel();
+
+			ChartSeries series;
+
+			barChartFlag = false;
+			if (listaProvasRealizadas.size() != 0) {
+				for (ProvaRealizadaDTO provaRealizadaDTO : listaProvasRealizadas) {
+					series = new ChartSeries();
+					series.setLabel(provaRealizadaDTO.getTituloProva());
+					series.set(
+							"Nota",
+							((provaRealizadaDTO.getQuantidadeAcertos()
+									.doubleValue() / provaRealizadaDTO
+									.getQuantidadeQuestoes().doubleValue()) * 100.00));
+					categoryModel.addSeries(series);
+				}
+				barChartFlag = true;
+			}
+
+		} catch (ClassNotFoundException e) {
+			LOG.error("Driver do banco de dados não encontrado", e);
+		} catch (SQLException e) {
+			LOG.error("Houve um problema na query do banco de dados", e);
+		}
+	}
+
+	public void carregarDashboard(ActionEvent event) {
+
+		try {
+			FacesContext context = FacesContext.getCurrentInstance();
+			HttpSession session = (HttpSession) context.getExternalContext()
+					.getSession(true);
+			FuncionarioDTO funcionario = (FuncionarioDTO) session
+					.getAttribute("funcionario");
+
+			DashboardService dashboardService = new DashboardServiceImpl();
+			listaAgendas = dashboardService.listarAgendas(funcionario
+					.getMatricula());
+
+		} catch (ClassNotFoundException e) {
+			LOG.error("Driver do banco de dados não encontrado", e);
+		} catch (SQLException e) {
+			LOG.error("Houve um problema na query do banco de dados", e);
+		}
+	}
+
+	@SuppressWarnings("unused")
+	public void carregarArquivo() {
+
+		try {
+			String filePath = apostilaSelecionada.getServerPath()
+					+ apostilaSelecionada.getHashName() + "_"
+					+ apostilaSelecionada.getNome();
+
+			String fileName = apostilaSelecionada.getNome();
+
+			String contentType = FacesContext.getCurrentInstance()
+					.getExternalContext().getMimeType(filePath);
+
+			File file = new File(filePath);
+
+			HttpServletResponse response = (HttpServletResponse) FacesContext
+					.getCurrentInstance().getExternalContext().getResponse();
+			response.setContentType(contentType);
+			response.setHeader("Content-Disposition",
+					String.format("attachment; filename=%s", fileName));
+			response.setContentLength((int) file.length());
+
+			InputStream fis = new FileInputStream(file);
+			ServletOutputStream out = response.getOutputStream();
+			byte[] buf = new byte[1024];
+			int i = 0;
+
+			while ((i = fis.read(buf)) != -1) {
+				out.write(buf);
+				out.flush();
+			}
+			fis.close();
+
+			FacesContext.getCurrentInstance().responseComplete();
+			response.getOutputStream().close();
+
+			LOG.info("Nome do arquivo: " + fileName);
+			LOG.info("Content Type: " + contentType);
+
+		} catch (IOException e) {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_FATAL, "Erro",
+							"Houve um erro na aplicação, tente mais tarde"));
+			LOG.error("Houve um erro ao processar o arquivo para download", e);
+		}
+
+	}
+
+	public String carregarProva() {
+
+		LOG.info("redirecionando: " + provaSelecionada.getProvaId());
+		try {
+			DashboardService dashboardService = new DashboardServiceImpl();
+			listaQuestoes = dashboardService
+					.listarQuestoesPorProva(provaSelecionada);
+		} catch (ClassNotFoundException e) {
+			LOG.error("Driver do banco de dados não encontrado", e);
+		} catch (SQLException e) {
+			LOG.error("Houve um problema na query do banco de dados", e);
+		}
+
+		return "/pages/system/realizar_prova";
+	}
+
+	public ApostilaDTO getApostilaSelecionada() {
+		return apostilaSelecionada;
+	}
+
+	public void setApostilaSelecionada(ApostilaDTO apostilaSelecionada) {
+		this.apostilaSelecionada = apostilaSelecionada;
+	}
+
+	public ProvaDTO getProvaSelecionada() {
+		return provaSelecionada;
+	}
+
+	public void setProvaSelecionada(ProvaDTO provaSelecionada) {
+		this.provaSelecionada = provaSelecionada;
+	}
+
+	public List<AgendaDTO> getListaAgendas() {
+		return listaAgendas;
+	}
+
+	public void setListaAgendas(List<AgendaDTO> listaAgendas) {
+		this.listaAgendas = listaAgendas;
+	}
+
+	public List<QuestaoDTO> getListaQuestoes() {
+		return listaQuestoes;
+	}
+
+	public void setListaQuestoes(List<QuestaoDTO> listaQuestoes) {
+		this.listaQuestoes = listaQuestoes;
+	}
+
+	public CartesianChartModel getCategoryModel() {
+		return categoryModel;
+	}
+
+	public Boolean getBarChartFlag() {
+		return barChartFlag;
+	}
+
+	public void setBarChartFlag(Boolean barChartFlag) {
+		this.barChartFlag = barChartFlag;
+	}
+
+}
